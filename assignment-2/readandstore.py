fd9bba889e6bf8b5c0609aa24d1688b9f65443e7
rdharavath-evoke <rdharavath@evoketechnologies.com>
2021-07-26 18:41:06 +0530

    readpdf and storeexcel

--- assignment-2/readandstore.py
+++ assignment-2/readandstore.py
@@ -1,4 +1,3 @@
-
 import re
 import logging
 import tabula
@@ -10,10 +9,9 @@ logging.basicConfig(level=logging.DEBUG)
 wb=openpyxl.load_workbook(r"D:\py-assignments-new\py-assignments\assignment-2\pdfreader.xlsx")
 ws=wb.active
 
-
 file=r"D:\py-assignments-new\py-assignments\assignment-2\tableform.pdf"
 data=tabula.read_pdf(file,pages=1)[0]
-print(data)
+
 
 data.to_excel(r"D:\py-assignments-new\py-assignments\assignment-2\pdfreader.xlsx",index=False)
 
@@ -23,23 +21,35 @@ for i in range(2,40):
     numbers=ws.cell(row=i,column=3).value
     number=str(numbers)
 
-    if re.compile(r'[01]\d\d\d\d\d\d\d\d\d\d').search(number) and re.match(r'0',number):
+    if re.compile(r'[01]\d{10}').search(number):
         ws['D{}'.format(i)]='Helpline_No'
         ws['D{}'.format(i)].font=Font(color="00FFFF")
-    elif re.compile(r'(\d\d\d)-(\d\d\d\d\d\d\d\d)').search(number):
+
+    elif re.compile(r'\d{3,5}-\d{6,8}').search(number):
         ws['D{}'.format(i)]='Landline_No.'
         ws['D{}'.format(i)].font=Font(color="008000")
-    elif re.compile(r'(\d\d\d\d)-(\d\d\d\d\d\d\d)').search(number):
-        ws['D{}'.format(i)]='Landline_No.'
-        ws['D{}'.format(i)].font=Font(color="008000")
-    elif re.compile(r'[36789]\d\d\d\d\d\d\d\d\d').search(number):
+    
+    elif re.compile(r'[36789]\d{9}').search(number):
         ws['D{}'.format(i)]='Mobile_No.'
         ws['D{}'.format(i)].font=Font(color="0000FF")
-    elif re.compile(r'(\d\d\d)').search(number):
+
+    elif re.compile(r'\d{3}').search(number):
         ws['D{}'.format(i)]='Emergency_Ambulance_No.'
         ws['D{}'.format(i)].font=Font(color="FF0000")
+
     else:
         ws['D{}'.format(i)]='None'
 
-wb.save(r"D:\py-assignments-new\py-assignments\assignment-2\pdfreader.xlsx")
 
+for i in range(2,40):
+    names=ws.cell(row=i,column=2).value
+    name=str(names)
+
+    if re.compile(r'^[M].*[h]$').search(name):
+        ws['E{}'.format(i)]=name
+        ws['E{}'.format(i)].font=Font(color="FF0000")
+    else:
+        ws['E{}'.format(i)]='None'  
+        ws['E{}'.format(i)].font=Font(color="0000FF")      
+
+wb.save(r"D:\py-assignments-new\py-assignments\assignment-2\pdfreader.xlsx")
