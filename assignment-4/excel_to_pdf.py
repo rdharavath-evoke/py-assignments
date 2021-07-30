9fe591b8ead52d93a1805e46e4e323defb5e7aa9
rdharavath-evoke <rdharavath@evoketechnologies.com>
2021-07-30 13:47:38 +0530

    excel-to-pdf

--- assignment-4/excel_to_pdf.py
+++ assignment-4/excel_to_pdf.py
@@ -1,6 +1,8 @@
 import openpyxl
 import logging
 import pandas as pd
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -13,9 +15,15 @@ ws=wb.active
 for c1,c2,c3 in ws['A1':'C29']:
     logging.debug("    {0:29} {1:29} {2:29}  ".format(c1.value,c2.value,c3.value))
 
+doc=SimpleDocTemplate("simple_table.pdf",pagesizes=letter)
 
 data=pd.ExcelFile(file)
-print(data.sheet_names)
-df=data.parse('Sheet1')
-print(df.head(10))
 
+df=data.parse('Sheet1')
+vals=df.values
+data1=vals.tolist()
+
+elements=[]
+t=Table(data1)
+elements.append(t)
+doc.build(elements)
