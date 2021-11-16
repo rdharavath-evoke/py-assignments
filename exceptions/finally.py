4de258792df5c8258ee307f6b05655744714ee65
rdharavath-evoke <rdharavath@evoketechnologies.com>
2021-11-16 15:19:32 +0530

    python-exceptions

---
+++ exceptions/finally.py
@@ -0,0 +1,17 @@
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
+  
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
