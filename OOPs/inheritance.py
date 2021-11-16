274d298b714fb8af4a3d15c7d29967168bc8b711
rdharavath-evoke <rdharavath@evoketechnologies.com>
2021-11-16 14:50:29 +0530

    python-OOPs Concepts

---
+++ OOPs/inheritance.py
@@ -0,0 +1,25 @@
+# Create a class named Person, with firstname and lastname properties, and a printname method:
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+
+
+# Create a class named Student, which will inherit the properties and methods from the Person class:
+
+class Student(Person):
+  pass
+
+# Use the Student class to create an object, and then execute the printname method:
+
+x = Student("Mike", "Olsen")
+x.printname()
